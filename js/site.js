484fcec573dadc863a36d0227db37d39c12cee60
cbenson11 <67032200+cbenson11@users.noreply.github.com>
2021-08-09 22:31:43 -0500

    update 8-9-21

--- js/site.js
+++ js/site.js
@@ -1,4 +1,30 @@
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
+//get the values from the Page
+//starts or controller function
+function getValues() {
+    //get values from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+
+    //we call generateNumbers
+    let numbers = generateNumbers(startValue, endValue);
+    //we call displayNumbers
+}
+
+//generate numbers from startValue to endValue
+//logic function(s)
+function generateNumbers(startValue, endValue) {
+    let numbers = [];
+
+    //we want to get all numbers from start to end
+    for(let i = startValue; i <= endValue; i++) {
+        numbers.push(i);
+    }
+
+    return numbers;
+}
+
+//display even numbers in bold
+//displays or view functions
+function displayNumbers() {
+
 }
