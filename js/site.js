b7e184d155dbd1f660e8744a05b83a3e1bb7140e
cbenson11 <67032200+cbenson11@users.noreply.github.com>
2021-08-11 22:05:30 -0500

    8-11-21 Update

--- js/site.js
+++ js/site.js
@@ -5,9 +5,19 @@ function getValues() {
     let startValue = document.getElementById("startValue").value;
     let endValue = document.getElementById("endValue").value;
 
-    //we call generateNumbers
-    let numbers = generateNumbers(startValue, endValue);
-    //we call displayNumbers
+    //We need to validate our input
+    //parse into Integers
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //we call generateNumbers
+        let numbers = generateNumbers(startValue, endValue);
+        //we call displayNumbers
+        displayNumbers(numbers);
+    }else{
+        alert("You must enter integers");
+    }
 }
 
 //generate numbers from startValue to endValue
@@ -25,6 +35,18 @@ function generateNumbers(startValue, endValue) {
 
 //display even numbers in bold
 //displays or view functions
-function displayNumbers() {
+function displayNumbers(numbers) {
+
+    let templateRows = "";
+
+    numbers.forEach(number => {
+        if (number % 2 == 0) {
+            templateRows += `<tr><td><strong>${number}</strong></td></tr>`
+        } else {
+            templateRows += `<tr><td>${number}</td></tr>`
+        }
+    });
+
+    document.getElementById("results").innerHTML = templateRows;
 
 }
